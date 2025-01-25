84fa0d4382843e128c9910241a911f36d2610c4f
Aidan <150118075+Kamchieva@users.noreply.github.com>
2025-01-25 10:22:50 -0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,19 +1,12 @@
-//package School.managment;
-//
-//import java.util.Arrays;
-//
-//public class Main {
-//    public static void main(String[] args) {
-//        Student student = new Student("Aida", 26, 1);
-//        Student student2 = new Student("Aidan", 27, 1);
-//        CourseManager courseManager = new CourseManager();
-//        courseManager.addStudent(student);
-//        courseManager.addStudent(student2);
-//        System.out.println(Arrays.toString(courseManager.getStudents()));
-//        student.enrollCourses("History");
-//        student2.enrollCourses( "History", "tennis", "english");
-//
-//
-//
-//    }
-//}
+import javax.swing.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    public static void main(String[] args) {
+
+     ContactForm form = new ContactForm();
+
+    }
+}
