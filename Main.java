f652d3f1f0733126b779dbedd842415b89e11769
Aidan <150118075+Kamchieva@users.noreply.github.com>
2025-01-26 23:24:35 -0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,12 +1,17 @@
-import javax.swing.*;
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-
     public static void main(String[] args) {
-
-     ContactForm form = new ContactForm();
-
+//   Computer comp = new Computer();
+//   comp.i7.start();
+//   comp.transfer.start();
+//   comp.transfer.shutdown();
+//    }
+        new Computer() {
+            void superComp (){
+                this.i7.start();
+                this.transfer.start();
+            }
+        };
     }
 }
