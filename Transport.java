4f20618d42962ca9d67d4bb0705491faa06214f6
Aidan <150118075+Kamchieva@users.noreply.github.com>
2025-02-02 20:48:28 -0600

    Add files via upload

--- Transport.java
+++ Transport.java
@@ -2,16 +2,14 @@ public class Transport {
 
     private float speed;
     private int weight;
-    protected String color;
+    private String color;
     private byte[] coordinate;
 
-
-
-    public Transport(float _speed, int _weight, String _color, byte[] _coordinate) {
+    public Transport (float speed, int weight, String color, byte[] coordinate) {
         // Join to DB
         System.out.println("Object created");
-        setValues(_speed, _weight, _color, _coordinate);
-        System.out.println(getValues());
+        setValues(speed, weight, color, coordinate);
+        System.out.println(this.getValues());
     }
 
     public Transport(int weight, byte[] coordinate) {
@@ -20,21 +18,21 @@ public class Transport {
         System.out.println(this.getValues());
     }
 
-    protected void setValues(float speed, int weight, String color, byte[] coordinate) {
-        this.speed = speed;
-        this.weight = weight;
-        this.color = color;
-        this.coordinate = coordinate;
+    public void setValues( float speed, int weight, String color, byte[] coordinate){
+      this.speed = speed;
+      this.weight = weight;
+      this.color = color;
+      this.coordinate = coordinate;
     }
 
     public String getValues() {
-        String info = "Object speed: " + this.speed + ". Weight: " + this.weight + ". Color: " + this.color + ".\n";
+        String info = "Object speed: " + speed + ". Weight: " + weight + ". Color: " + color + ". \n";
         String infoCoordinates = "Coordinates:\n";
-
-        for(int i = 0; i < coordinate.length; i++)
-            infoCoordinates += coordinate[i] + "\n";
+        for (int  i = 0; i < coordinate.length; i++)
+            infoCoordinates += coordinate [i] + "\n";
 
         return info + infoCoordinates;
-    }
 
+
+    }
 }
