fd96689d185bd582b6b0fd6b2e9039b4e9e63a5e
Aidan <150118075+Kamchieva@users.noreply.github.com>
2025-02-16 20:31:04 -0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,11 +1,36 @@
-import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
-      Scanner scanner = new Scanner(System.in);
-        System.out.println("What is your name?");
-        String name = scanner.nextLine();
+package DataS;
 
-        System.out.println("Hello" + name);
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Main {
+    public class rows{
+
+    }
+
+    public static void main(String[] args) {
+
+
+        String[][] array = {{"l", "l", "l","l","l","l","l","l","l"},
+                           {"l", "l", "l","l","l","l","l","l","l"},
+                           {"l", "l", "l","l","l","l","l","l","l"},
+                           {"l", "l", "l","l","l","l","l","l","l"},
+                           {"l", "l", "l","l","l","l","l","l","l"},
+                           {"l", "l", "l","l","l","l","l","l","l"} };
+
+       for(int i = 0; i < array.length; i++){
+
+           System.out.println(" ");
+
+           for(int j = 0; j < array[i].length;  j++) {
+
+
+               System.out.print(array[i][j] + " ");
+
+
+           }
+       }
+
 
     }
 }
