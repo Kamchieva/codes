c87c302fa39b4f4696065e0e78f2e55a231c980f
Aidan <150118075+Kamchieva@users.noreply.github.com>
2025-02-26 18:07:03 -0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,19 +1,28 @@
-import java.util.HashMap;
+ abstract class Dog{
 
-public class Main {
-    public static void main(String[] args) {
-        HashMap<String, Integer> empIds = new HashMap<>() ;
-        empIds.put("John", 12345) ;
-        empIds.put("Carl", 54321) ;
-        empIds.put("Jerry", 455664455) ;
-
-        System.out.println(empIds) ;
-        System.out.println(empIds.containsValue(12345)) ;
-        empIds.put("John", 3333) ;
-        empIds.replace("Karen", 777) ;
-
-        empIds.putIfAbsent("Steve",222);
-        empIds.remove("Steve");
-        System.out.println(empIds) ;
+    public void bark(){
+        System.out.println("Bark!");
     }
+    public abstract void poop();
 }
+
+   class Chihuahua extends Dog{
+       @Override
+       public void poop() {
+           System.out.println("Dog pooped!");
+       }
+   }
+
+
+
+    public class Main{
+
+    public static void main(String[] args) {
+
+        Chihuahua d = new Chihuahua();
+        d.bark();
+        d.poop();
+
+
+    }
+    }
