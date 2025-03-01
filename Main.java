4af0e35ee2097542d2cc45f2c2653b105e8fac48
Aidan <150118075+Kamchieva@users.noreply.github.com>
2025-03-01 16:06:33 -0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,17 +1,23 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+class Node {
+    int data ;
+    Node next ;
+    Node(int data)  {
+        this.data = data ;
+    }
+    int countNodes(Node head) {
+ int count = 1;
+ Node current = head;
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+ while(current.next != null) {
+     current = current . next ;
+     count += 1;
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-        }
+ }
+        return count;
     }
 }
+    public static void main(String[] args) {
+
+    }
