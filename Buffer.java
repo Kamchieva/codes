be91be7f48e3f8b1ef01b6e625f4baa4abc9d30b
Aidan <150118075+Kamchieva@users.noreply.github.com>
2025-04-26 23:49:22 -0500

    Add files via upload

--- Buffer.java
+++ Buffer.java
@@ -1,19 +1,34 @@
 package org.example;
-
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.util.Scanner;
 
 public class Buffer {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
-            writer.write("Welcome!!!");
-            writer.close();
-        } catch(IOException e){
-            e.printStackTrace();
+        while (true) {
+            System.out.print("> ");
+            String line = scanner.nextLine();
+            String[] tokens = line.split(" ");
+            String command = tokens[0];
+
+            switch (command) {
+                case "PRINT":
+                    System.out.println(tokens[1]);
+                    break;
+                case "ADD":
+                    int sum = Integer.parseInt(tokens[1]) + Integer.parseInt(tokens[2]);
+                    System.out.println("Result: " + sum);
+                    break;
+                case "SUB":
+                    int diff = Integer.parseInt(tokens[1]) - Integer.parseInt(tokens[2]);
+                    System.out.println("Result: " + diff);
+                    break;
+                case "EXIT":
+                    System.out.println("Bye!");
+                    return;
+                default:
+                    System.out.println("Unknown command");
+            }
         }
-
     }
 }
