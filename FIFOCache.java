958840c9aa4a162f0931840bc097402ecf712512
Aidan <150118075+Kamchieva@users.noreply.github.com>
2025-06-20 21:59:17 -0500

    Add files via upload

---
+++ FIFOCache.java
@@ -0,0 +1,30 @@
+package CacheImplement;
+
+import Interfaces.ICache;
+
+public class FIFOCache implements ICache{
+
+    @Override
+    public void put(String key, int value) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'put'");
+    }
+
+    @Override
+    public int get(String key) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'get'");
+    }
+
+    @Override
+    public void remove(String key) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'remove'");
+    }
+
+    public void setCapacity(int capacity) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setCapacity'");
+    }
+    public void setCapacity(int capacity);
+}
