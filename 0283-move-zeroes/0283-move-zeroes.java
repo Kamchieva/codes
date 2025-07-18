6804b3895205abbb4a09e5c517c1793b92a6a7f3
Aidan <150118075+Kamchieva@users.noreply.github.com>
2025-07-17 21:53:42 -0500

    Time: 1 ms (99.76%), Space: 46.6 MB (9.21%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,17 +1,19 @@
 class Solution {
-    public void moveZeroes(int[] nums) {
-       if(nums == null || nums.length == 0){
-        return;
-       } 
-       int insertPosition = 0;
-       for(int x : nums){
-        if(x != 0){
-            nums[insertPosition] = x;
-            insertPosition++;
-        }
-       }
-       for(; insertPosition < nums.length; insertPosition++){
-        nums[insertPosition] = 0;
-       }
+    public void moveZeroes(int[] nums) {   
+if(nums == null || nums.length == 0){
+    return;
+}
+
+int InsertPosition = 0;
+for(int x : nums){
+    if(x != 0) {
+nums[InsertPosition] = x;
+InsertPosition++;
+    }
+}
+
+for(;InsertPosition < nums.length; InsertPosition++){
+    nums[InsertPosition] = 0;
+}
     }
 }
